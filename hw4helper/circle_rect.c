b7619efa4fe8dbb0a8ed559f27e3ae30774bca68
millieyan <millie111111@yahoo.com.tw>
2019-05-10 03:04:13 +0800

    hw4

---
+++ hw4helper/circle_rect.c
@@ -0,0 +1,39 @@
+#include "GraphicsGems.h"
+#include <stdio.h>
+boolean Check_Intersect(R, C, Rad)
+ Box2 *R;
+ Point2 *C;
+ double Rad;
+{
+ double Rad2;
+
+ Rad2 = Rad * Rad;
+
+ R->max.x -= C->x;  R->max.y -= C->y;
+ R->min.x -= C->x;  R->min.y -= C->y;
+
+ if (R->max.x < 0) 			/* R to left of circle center */
+   	if (R->max.y < 0) 		/* R in lower left corner */
+     		return ((R->max.x * R->max.x + R->max.y * R->max.y) < Rad2);
+   	else if (R->min.y > 0) 	/* R in upper left corner */
+     		return ((R->max.x * R->max.x + R->min.y * R->min.y) < Rad2);
+   	else 					/* R due West of circle */
+     		return(ABS(R->max.x) < Rad);
+ 	else if (R->min.x > 0)  	/* R to right of circle center */
+   		if (R->max.y < 0) 	/* R in lower right corner */
+     			return ((R->min.x * R->min.x + R->max.y * R->max.y) < Rad2);
+   	else if (R->min.y > 0)  	/* R in upper right corner */
+     		return ((R->min.x * R->min.x + R->min.y * R->min.y) < Rad2);
+   	else 				/* R due East of circle */
+     		return (R->min.x < Rad);
+ 	else				/* R on circle vertical centerline */
+   		if (R->max.y < 0) 	/* R due South of circle */
+     		return (ABS(R->max.y) < Rad);
+   	else if (R->min.y > 0)  	/* R due North of circle */
+     		return (R->min.y < Rad);
+   	else 				/* R contains circle centerpoint */
+     		return(TRUE);
+} 	
+int main(int argc,char * argc[]){
+	
+}
